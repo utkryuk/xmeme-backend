eb239500b51466310ddd758150b59015105e4031
utkryuk <utkryuk@gmail.com>
2021-02-14 16:21:17 +0530

    added https

--- index.js
+++ index.js
@@ -53,9 +53,16 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const app = require('./app')
-const http = require('http')
+// const http = require('http')
+const https = require('https')
+const fs = require('fs')
 
-const server = http.createServer(app)
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}
+
+const server = https.createServer(options, app)
 
 server.listen(config.PORT, () => {
     logger.info(`Server running on PORT ${config.PORT}`)
