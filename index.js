8a96d2f286946754df28ed6da4d27f50d94a7f21
utkryuk <utkryuk@gmail.com>
2021-02-14 19:08:39 +0530

    changed back to previous commit

--- index.js
+++ index.js
@@ -54,16 +54,16 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const app = require('./app')
 // const http = require('http')
-const https = require('https')
-const fs = require('fs')
+// const https = require('https')
+// const fs = require('fs')
 
-const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-}
+// const options = {
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync('cert.pem')
+// }
 
-const server = https.createServer(options, app)
+// const server = https.createServer(options, app)
 
-server.listen(config.PORT, () => {
+app.listen(config.PORT, () => {
     logger.info(`Server running on PORT ${config.PORT}`)
 })
